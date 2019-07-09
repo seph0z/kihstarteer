f8710b2923eac7c55dee5370844aff887c440f18
seph0z <murashkevich101@gmail.com>
2019-07-09 20:04:37 +0300

    With IRepository

---
+++ Kehstartir/Kehstartir.Data.Contracts/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kehstartir.Data.Contracts
+{
+    public interface IRepository<T>
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(int id);
+        void SaveChanges();
+    }
+}
