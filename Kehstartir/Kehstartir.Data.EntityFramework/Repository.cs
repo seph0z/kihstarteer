590e4b64a4f768d0fae5e4de337a38a50155936c
seph0z <murashkevich101@gmail.com>
2019-07-09 20:20:16 +0300

    y

--- Kehstartir/Kehstartir.Data.EntityFramework/Repository.cs
+++ Kehstartir/Kehstartir.Data.EntityFramework/Repository.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using Kehstartir.Data.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Kehstartir.Data.EntityFramework
 {
-    class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        private readonly EntitiesContext entitiesContext;
+        public Repository(EntitiesContext entitiesContext)
+        {
+            this.entitiesContext = entitiesContext;
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
